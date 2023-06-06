b78b1236540439fbae6840fbdcf5096c78d858a7
aleksandra-gab <thatsaleksandra@gmail.com>
2023-06-06 18:05:33 +0100

    Added JavaScript

--- src/app.js
+++ src/app.js
@@ -1,1 +1,13 @@
+function buy() {
+  let name = prompt("What is your name?");
+  let email = prompt("What is your email address?");
 
+  if (name.length > 0) {
+    alert(
+      `Thank you, ${name}. We'll send a confirmation email to ${email}. Have fun at the beach! 👙`
+    );
+  }
+}
+
+let buyButton = document.querySelector(".buy-button");
+buyButton.addEventListener("click", buy);
